c18c4b962123b043b229e5388ea55476f1d75aa1
tamiressantiago <tamireszsantiago@gmail.com>
2026-03-17 16:37:35 -0300

    fix: test_train.py

--- tests/test_train.py
+++ tests/test_train.py
@@ -5,10 +5,10 @@ import torch.nn as nn
 from src.train import train_epoch, eval_epoch
 
 class MockModel(nn.Module):
-
-    def __init__(self):
+    def __init__(self, model_type="r"):
         super().__init__()
         self.layer = nn.Linear(10, 2)
+        self.type = model_type
 
     def forward(self, x):
         return self.layer(x)
@@ -17,23 +17,23 @@ class ConstantLoss(nn.Module):
     """
     Função de perda que retorna sempre um valor constante. 
     """
-
     def forward(self, outputs, labels):
         return torch.tensor(5.0, requires_grad=True)
 
 class TestEpochLossCalculations(unittest.TestCase):
-
     def setUp(self):
         self.device = torch.device("cpu")
-        self.model = MockModel().to(self.device)
+        self.model = MockModel(model_type="r").to(self.device)
         self.criterion = ConstantLoss()
-        
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.1)
 
         self.mock_loader = []
         for _ in range(4):
             images = torch.randn(2, 10)
-            labels = torch.randn(2)
+            age = torch.randn(2)
+            age_class = torch.randint(0, 5, (2,))
+            labels = (age, age_class)
+            
             self.mock_loader.append((images, labels))
 
     def test_train_loss_calculation(self):
